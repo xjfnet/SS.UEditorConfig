e793e964a8607e3754dfe5a3b2b4a791b78dd550
QQ56188788 <56188788@QQ.COM>
2020-08-21 22:57:32 +0800

    Initial commit

---
+++ SS.UEditorConfig/UEditorConfig.cs
@@ -0,0 +1,31 @@
+﻿using HookingDotNet;
+using Newtonsoft.Json.Linq;
+using SiteServer.Plugin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SS.UEditorConfig
+{
+    public class UEditorConfig : PluginBase
+    {
+        private static Dictionary<string, Interceptor> Interceptors = new Dictionary<string, Interceptor>();
+        
+        public override void Startup(IService service)
+        {
+            Interceptors["UEditorConfig"] = new Interceptor(
+                typeof(SiteServer.CMS.UEditor.Config).GetMethod("BuildItems", BindingFlags.Static | BindingFlags.NonPublic),
+                typeof(UEditorConfig).GetMethod("BuildItems"));
+        }
+
+        public static JObject BuildItems()
+        {
+            var file = Path.Combine(Context.Environment.PhysicalApplicationPath, @"SiteServer\assets\ueditor\config.json");
+            return Context.UtilsApi.JsonDeserialize<dynamic>(File.ReadAllText(file));
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,32 @@
+﻿using HookingDotNet;
+using System;
+
+namespace Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var interceptor = new Interceptor(typeof(Target).GetMethod("Get"), typeof(Program).GetMethod("Get"));
+
+            //var target = new Target();
+            //target.Get();
+            Target.Get();
+
+            interceptor.Invoke(() => Target.Get());
+        }
+
+        public static void Get()
+        {
+            Console.WriteLine("111111111111111");
+        }
+    }
+
+    public static class Target
+    {
+        public static void Get()
+        {
+            Console.WriteLine("22222222222222");
+        }
+    }
+}
